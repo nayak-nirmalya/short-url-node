2e358af2a245011c3778f393858018ecfdb9f53c
Nirmalya Nayak <n.nayak263@gmail.com>
2023-08-07 20:35:13 +0530

    new controller to handleGenerateNewShortURL

--- controllers/url.js
+++ controllers/url.js
@@ -1,1 +1,20 @@
-async function handleGenerateNewShortURL(req, res) {}
+const nanoid = require("nanoid");
+const URL = require("../models/url");
+
+async function handleGenerateNewShortURL(req, res) {
+  const body = req.body;
+
+  if (!body.url) return res.status(400).json({ error: "URL is Required." });
+
+  const shortId = nanoid(8);
+
+  await URL.create({
+    shortId,
+    redirectURL: body.url,
+    visitHistory: [],
+  });
+
+  return res.json({ id: shortId });
+}
+
+module.exports = { handleGenerateNewShortURL };
