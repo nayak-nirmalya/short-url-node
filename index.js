083783097350a50f7c8a7cc3423e5e2c18146ae2
Nirmalya Nayak <n.nayak263@gmail.com>
2023-08-07 21:29:42 +0530

    return full url on GET request with valid id

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config();
 
 const urlRoute = require("./routes/url");
+const URL = require("./models/url");
 const { connectToMongoDB } = require("./connnect");
 
 const app = express();
@@ -13,6 +14,22 @@ connectToMongoDB(mongoDB_URL).then(() => console.log("MongoDB Connected."));
 app.use(express.json());
 
 app.use("/url", urlRoute);
+
+app.get("/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId,
+    },
+    {
+      $push: {
+        visitHistory: { timestamp: Date.now() },
+      },
+    }
+  );
+
+  return res.json({ url: entry.redirectURL });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is Running on PORT: ${PORT}`);
