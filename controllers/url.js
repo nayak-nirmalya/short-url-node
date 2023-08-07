2706e0649d561c6f9faf32094a55e7096a68c85c
Nirmalya Nayak <n.nayak263@gmail.com>
2023-08-07 21:37:13 +0530

    added new route and controller handleGetAnalytics

--- controllers/url.js
+++ controllers/url.js
@@ -18,4 +18,14 @@ async function handleGenerateNewShortURL(req, res) {
   return res.json({ id: shortId });
 }
 
-module.exports = { handleGenerateNewShortURL };
+async function handleGetAnalytics(req, res) {
+  const shortId = req.params.shortId;
+  const result = await URL.findOne({ shortId });
+
+  return res.json({
+    totalClicks: result.visitHistory.length,
+    analytics: result.visitHistory,
+  });
+}
+
+module.exports = { handleGenerateNewShortURL, handleGetAnalytics };
